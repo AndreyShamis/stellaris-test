9628f2f3f7e5642495b69a79aa43c189de31210f
Andrey Shamis <lolnik@gmail.com>
2013-10-29 01:16:57 +0200

    Send Chip On Left Button Push
    Signed-off-by: Andrey Shamis <lolnik@gmail.com>

--- main.c
+++ main.c
@@ -1,35 +1,3 @@
-/*
-* Copyright (c) 2012, Mauro Scomparin
-* All rights reserved.
-*
-* Redistribution and use in source and binary forms, with or without
-* modification, are permitted provided that the following conditions are met:
-*     * Redistributions of source code must retain the above copyright
-*       notice, this list of conditions and the following disclaimer.
-*     * Redistributions in binary form must reproduce the above copyright
-*       notice, this list of conditions and the following disclaimer in the
-*       documentation and/or other materials provided with the distribution.
-*     * Neither the name of Mauro Scomparin nor the
-*       names of its contributors may be used to endorse or promote products
-*       derived from this software without specific prior written permission.
-*
-* THIS SOFTWARE IS PROVIDED BY Mauro Scomparin ``AS IS'' AND ANY
-* EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
-* WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-* DISCLAIMED. IN NO EVENT SHALL Mauro Scomparin BE LIABLE FOR ANY
-* DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
-* (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
-* LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
-* ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-* (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
-* SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-*
-* File:			main.c.
-* Author:		Mauro Scomparin <http://scompoprojects.worpress.com>.
-* Version:		1.0.0.
-* Description:	Main sample file.
-*/
-
 #include "inc/hw_ints.h"
 #include "inc/hw_gpio.h"
 #include "inc/hw_memmap.h"
@@ -41,13 +9,15 @@
 #include "driverlib/timer.h"
 #include "driverlib/rom.h"
 
+
 #define LED_RED GPIO_PIN_1
 #define LED_BLUE GPIO_PIN_2
 #define LED_GREEN GPIO_PIN_3
 
 #define TX_PIN GPIO_PIN_7
 
-
+#define BUTTON_1 GPIO_PIN_0
+#define BUTTON_2 GPIO_PIN_4
 
 // Basically here I'm checking that everything works fine.
 volatile unsigned long count;
@@ -68,13 +38,33 @@ int main(void) {
 	count=3;
 	TimerEnable(TIMER1_BASE, TIMER_A);
 
-	ROM_SysCtlClockSet(SYSCTL_SYSDIV_4|SYSCTL_USE_PLL|SYSCTL_XTAL_16MHZ|SYSCTL_OSC_MAIN);
+
+	//ROM_SysCtlClockSet(SYSCTL_SYSDIV_4|SYSCTL_USE_PLL|SYSCTL_XTAL_16MHZ|SYSCTL_OSC_MAIN);
 	ROM_SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOF);
 
 	ROM_GPIOPinTypeGPIOOutput(GPIO_PORTF_BASE, LED_RED|LED_BLUE|LED_GREEN);
+	//ROM_GPIOPinTypeGPIOInput(GPIO_PORTF_BASE,GPIO_PIN_4);
+    // Unlock PF0 so we can change it to a GPIO input
+    // Once we have enabled (unlocked) the commit register then re-lock it
+    // to prevent further changes.  PF0 is muxed with NMI thus a special case.
+    HWREG(GPIO_PORTF_BASE + GPIO_O_LOCK) = GPIO_LOCK_KEY_DD;
+    HWREG(GPIO_PORTF_BASE + GPIO_O_CR) |= 0x01;
+    HWREG(GPIO_PORTF_BASE + GPIO_O_LOCK) = 0;
+    // set user switches as inputs
+    GPIOPinTypeGPIOInput(GPIO_PORTF_BASE, BUTTON_1|BUTTON_2);
+
+    // turn weak pull-ups on
+      GPIOPadConfigSet(GPIO_PORTF_BASE, BUTTON_1|BUTTON_2, GPIO_STRENGTH_2MA,
+      GPIO_PIN_TYPE_STD_WPU);
 
 	ROM_SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOA);
+
 	ROM_GPIOPinTypeGPIOOutput(GPIO_PORTA_BASE, TX_PIN);
+
+
+	//ROM_SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOF);
+
+
 	//GPIOPinTypeGPIOOutput(GPIO_PORTA_BASE, TX_PIN);
 
 	while(1)
@@ -84,25 +74,31 @@ int main(void) {
 			var_init++;
 			i+=1;
 			ROM_GPIOPinWrite(GPIO_PORTF_BASE, LED_RED|LED_GREEN|LED_BLUE, i);
-			ROM_SysCtlDelay(4000000);
+			ROM_SysCtlDelay(10000);
 			ROM_GPIOPinWrite(GPIO_PORTF_BASE, LED_RED|LED_GREEN|LED_BLUE, 0);
 			ROM_SysCtlDelay(1000000);
+			long val = GPIOPinRead(GPIO_PORTF_BASE,BUTTON_2);
+			if(!val)
+				SendChip();
 			if(i>=15)
 			{
 				i=0;
 				//ROM_GPIOPinWrite(GPIO_PORTA_BASE, TX_PIN,1);
-				GPIOPinWrite(GPIO_PORTA_BASE, TX_PIN , 0x80);
-				ROM_SysCtlDelay(10000000);
-				ROM_SysCtlDelay(10000000);
-				ROM_SysCtlDelay(10000000);
-				GPIOPinWrite(GPIO_PORTA_BASE, TX_PIN , 0x00);
-				ROM_SysCtlDelay(10000000);
+
 
 			}
 		}
     }
 }
 
+void SendChip()
+{
+	GPIOPinWrite(GPIO_PORTA_BASE, TX_PIN , 0x80);
+	ROM_SysCtlDelay(10000000);
+	ROM_SysCtlDelay(10000000);
+	ROM_SysCtlDelay(10000000);
+	GPIOPinWrite(GPIO_PORTA_BASE, TX_PIN , 0x00);
+}
 // The interrupt function definition.
 void Timer1A_ISR(void){
 	TimerIntClear(TIMER1_BASE, TIMER_TIMA_TIMEOUT);
