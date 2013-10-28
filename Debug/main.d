4a4dc786a7bb69168c80b57f6452c864df9fc3fe
Andrey Shamis <lolnik@gmail.com>
2013-10-28 23:59:11 +0200

    Set PIN GPIO_PIN_7 as TX PIN
    Signed-off-by: Andrey Shamis <lolnik@gmail.com>

---
+++ Debug/main.d
@@ -0,0 +1,33 @@
+main.d: ../main.c /home/tester/devel/stellaris-launchpad/inc/hw_ints.h \
+ /home/tester/devel/stellaris-launchpad/inc/hw_gpio.h \
+ /home/tester/devel/stellaris-launchpad/inc/hw_memmap.h \
+ /home/tester/devel/stellaris-launchpad/inc/hw_sysctl.h \
+ /home/tester/devel/stellaris-launchpad/inc/hw_types.h \
+ /home/tester/devel/stellaris-launchpad/driverlib/gpio.h \
+ /home/tester/devel/stellaris-launchpad/driverlib/pin_map.h \
+ /home/tester/devel/stellaris-launchpad/driverlib/sysctl.h \
+ /home/tester/devel/stellaris-launchpad/driverlib/interrupt.h \
+ /home/tester/devel/stellaris-launchpad/driverlib/timer.h \
+ /home/tester/devel/stellaris-launchpad/driverlib/rom.h
+
+/home/tester/devel/stellaris-launchpad/inc/hw_ints.h:
+
+/home/tester/devel/stellaris-launchpad/inc/hw_gpio.h:
+
+/home/tester/devel/stellaris-launchpad/inc/hw_memmap.h:
+
+/home/tester/devel/stellaris-launchpad/inc/hw_sysctl.h:
+
+/home/tester/devel/stellaris-launchpad/inc/hw_types.h:
+
+/home/tester/devel/stellaris-launchpad/driverlib/gpio.h:
+
+/home/tester/devel/stellaris-launchpad/driverlib/pin_map.h:
+
+/home/tester/devel/stellaris-launchpad/driverlib/sysctl.h:
+
+/home/tester/devel/stellaris-launchpad/driverlib/interrupt.h:
+
+/home/tester/devel/stellaris-launchpad/driverlib/timer.h:
+
+/home/tester/devel/stellaris-launchpad/driverlib/rom.h:
