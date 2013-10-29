0b56b39327e4fcd2b6e4b4d0e32b3d5d801d7776
Andrey Shamis <lolnik@gmail.com>
2013-10-29 12:53:37 +0200

    Use ROM functions
    Signed-off-by: Andrey Shamis <lolnik@gmail.com>

--- main.c
+++ main.c
@@ -9,13 +9,10 @@
 #include "driverlib/timer.h"
 #include "driverlib/rom.h"
 
-
 #define LED_RED GPIO_PIN_1
 #define LED_BLUE GPIO_PIN_2
 #define LED_GREEN GPIO_PIN_3
-
 #define TX_PIN GPIO_PIN_7
-
 #define BUTTON_1 GPIO_PIN_0
 #define BUTTON_2 GPIO_PIN_4
 
@@ -28,21 +25,19 @@ void Timer1A_ISR(void);
 
 // main function.
 int main(void) {
-	SysCtlClockSet(SYSCTL_SYSDIV_2_5|SYSCTL_USE_PLL|SYSCTL_OSC_MAIN|SYSCTL_XTAL_16MHZ);
-	SysCtlPeripheralEnable(SYSCTL_PERIPH_TIMER1);
-	TimerConfigure(TIMER1_BASE, TIMER_CFG_SPLIT_PAIR | TIMER_CFG_A_PERIODIC);
-	TimerControlStall(TIMER1_BASE, TIMER_A, true);
-	TimerLoadSet(TIMER1_BASE, TIMER_A, 2111);
+	ROM_SysCtlClockSet(SYSCTL_SYSDIV_2_5|SYSCTL_USE_PLL|SYSCTL_OSC_MAIN|SYSCTL_XTAL_16MHZ);
+	ROM_SysCtlPeripheralEnable(SYSCTL_PERIPH_TIMER1);
+	ROM_TimerConfigure(TIMER1_BASE, TIMER_CFG_SPLIT_PAIR | TIMER_CFG_A_PERIODIC);
+	ROM_TimerControlStall(TIMER1_BASE, TIMER_A, true);
+	ROM_TimerLoadSet(TIMER1_BASE, TIMER_A, 2111);
 	TimerIntRegister(TIMER1_BASE, TIMER_A, Timer1A_ISR);
-	TimerIntEnable(TIMER1_BASE, TIMER_TIMA_TIMEOUT);
+	ROM_TimerIntEnable(TIMER1_BASE, TIMER_TIMA_TIMEOUT);
 	count=3;
-	TimerEnable(TIMER1_BASE, TIMER_A);
+	ROM_TimerEnable(TIMER1_BASE, TIMER_A);
 
+	ROM_SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOF);						//	PORT F
 
-	//ROM_SysCtlClockSet(SYSCTL_SYSDIV_4|SYSCTL_USE_PLL|SYSCTL_XTAL_16MHZ|SYSCTL_OSC_MAIN);
-	ROM_SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOF);
-
-	ROM_GPIOPinTypeGPIOOutput(GPIO_PORTF_BASE, LED_RED|LED_BLUE|LED_GREEN);
+	ROM_GPIOPinTypeGPIOOutput(GPIO_PORTF_BASE, LED_RED|LED_BLUE|LED_GREEN);	//	PORT F LED
 
 	// Unlock PF0 so we can change it to a GPIO input
     // Once we have enabled (unlocked) the commit register then re-lock it
@@ -50,16 +45,12 @@ int main(void) {
     HWREG(GPIO_PORTF_BASE + GPIO_O_LOCK) = GPIO_LOCK_KEY_DD;
     HWREG(GPIO_PORTF_BASE + GPIO_O_CR) |= 0x01;
     HWREG(GPIO_PORTF_BASE + GPIO_O_LOCK) = 0;
-
     // set user switches as inputs
-    GPIOPinTypeGPIOInput(GPIO_PORTF_BASE, BUTTON_1|BUTTON_2);
-
+    ROM_GPIOPinTypeGPIOInput(GPIO_PORTF_BASE, BUTTON_1|BUTTON_2);			//	PORT F BUTTONS
     // turn weak pull-ups on
-    GPIOPadConfigSet(GPIO_PORTF_BASE, BUTTON_1|BUTTON_2, GPIO_STRENGTH_2MA,GPIO_PIN_TYPE_STD_WPU);
-
-	ROM_SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOA);
-
-	ROM_GPIOPinTypeGPIOOutput(GPIO_PORTA_BASE, TX_PIN);
+    ROM_GPIOPadConfigSet(GPIO_PORTF_BASE, BUTTON_1|BUTTON_2, GPIO_STRENGTH_2MA,GPIO_PIN_TYPE_STD_WPU);
+	ROM_SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOA);						// PORT A
+	ROM_GPIOPinTypeGPIOOutput(GPIO_PORTA_BASE, TX_PIN);						// PORT A RF_TX_PIN
 	while(1)
     {	
 		int i = 0;
@@ -70,7 +61,7 @@ int main(void) {
 			ROM_SysCtlDelay(1000);
 			ROM_GPIOPinWrite(GPIO_PORTF_BASE, LED_RED|LED_GREEN|LED_BLUE, 0);
 			ROM_SysCtlDelay(1000000);
-			long val = GPIOPinRead(GPIO_PORTF_BASE,BUTTON_2);
+			long val = ROM_GPIOPinRead(GPIO_PORTF_BASE,BUTTON_2);
 			if(!val)
 				SendChip();
 			if(i>=15)
@@ -83,14 +74,14 @@ int main(void) {
 
 void SendChip()
 {
-	GPIOPinWrite(GPIO_PORTA_BASE, TX_PIN , 0x80);
+	ROM_GPIOPinWrite(GPIO_PORTA_BASE, TX_PIN , 0x80);
 	ROM_SysCtlDelay(10000000);
-	ROM_SysCtlDelay(10000000);
-	ROM_SysCtlDelay(10000000);
-	GPIOPinWrite(GPIO_PORTA_BASE, TX_PIN , 0x00);
+	ROM_GPIOPinWrite(GPIO_PORTA_BASE, TX_PIN , 0x00);
 }
+
 // The interrupt function definition.
-void Timer1A_ISR(void){
+void Timer1A_ISR(void)
+{
 	TimerIntClear(TIMER1_BASE, TIMER_TIMA_TIMEOUT);
 	count++;
 }
